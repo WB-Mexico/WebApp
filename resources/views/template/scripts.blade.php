9fdf5187ad3b5518ba3efef468e65164d461d1d3
Miguel Cazares <miguel;soledad751@gmail.com>
2022-08-06 01:19:28 -0500

    add template

---
+++ resources/views/template/scripts.blade.php
@@ -0,0 +1,7 @@
+ <!-- Bootstrap core JavaScript-->
+ <script src="{{asset('template/assets/js/jquery.min.js')}}"></script>
+ <script src="{{asset('template/assets/js/bootstrap.bundle.min.js')}}"></script>
+ <!-- Core plugin JavaScript-->
+ <script src="{{asset('template/assets/js/jquery.easing.min.js')}}"></script>
+ <!-- Custom scripts for all pages-->
+ <script src="{{asset('template/assets/js/sb-admin-2.min.js')}}"></script>
